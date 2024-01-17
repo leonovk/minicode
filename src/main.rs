4e3761974ae637ad00b9ae54105d93479a94e5d2
Kirill Leonov <leonov835@yandex.ru>
2024-01-17 17:31:15 +0300

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::Parser;
+
+/// My lang
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// file path
+    #[arg(short, long)]
+    path: Option<String>,
+
+    /// update command
+    #[arg(short, long)]
+    update: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if cli.update {
+        self_update();
+        return;
+    }
+
+    if cli.path != None {
+        println!("run code");
+    } else {
+        println!("enter the command --help or any other command");
+    }
+}
+
+fn self_update() {
+    println!("updating");
 }
