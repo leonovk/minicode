bd5235ab77740400160f7c7cf69b9fcfc716fa00
Kirill Leonov <leonov835@yandex.ru>
2024-01-18 21:36:12 +0300

    todo rework

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -2,9 +2,15 @@ use crate::opcode::OpCode;
 use crate::opcode::OpCode::*;
 
 pub fn exegete(operations: Vec<OpCode>) {
-    eval();
+    for operation in operations {
+        eval(operation);
+    }
 }
 
-pub fn eval() {
-    println!("eval")
+pub fn eval(operation: OpCode) {
+    match operation {
+        Create(_k, _v) => todo!(),
+        Print(_k) => todo!(),
+        ErrorCode(e) => panic!("{}", e)
+    }
 }
--- src/self_update.rs
+++ src/self_update.rs
@@ -1,3 +1,3 @@
 pub fn update() {
-    println!("updating");
+    todo!();
 }
