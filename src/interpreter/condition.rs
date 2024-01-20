b5df7eb6fe2d18e5acd9dcf82d3253c953c7d186
Кирилл Леонов <71232234+leonovk@users.noreply.github.com>
2024-01-20 21:28:22 +0300

    🔨 Need more tests (#4)

--- src/interpreter/condition.rs
+++ src/interpreter/condition.rs
@@ -24,3 +24,27 @@ pub fn condition(
         None => panic!("condition - not value"),
     };
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_condition_true_int() {
+        let mut map: HashMap<&String, ValueType> = HashMap::new();
+        let binding = String::from("test_key");
+        map.insert(&binding, Int(10));
+        let result = condition(&String::from("test_key"), &true, &10, &map);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_condition_false_int() {
+        let mut map: HashMap<&String, ValueType> = HashMap::new();
+        let binding = String::from("test_key");
+        map.insert(&binding, Int(10));
+        let result = condition(&String::from("test_key"), &false, &5, &map);
+        assert_eq!(result, true);
+    }
+}
--- src/parser/appropriation.rs
+++ src/parser/appropriation.rs
@@ -3,10 +3,65 @@ use crate::opcode::OpCode::*;
 use crate::opcode::ValueType::*;
 
 pub fn appropriation(data: Vec<&str>) -> OpCode {
+    if data.len() < 3 {
+        return ErrorCode("the operation is not specified correctly".to_string());
+    }
+
     let value_name = data[1].to_string();
     let value: String = data.into_iter().skip(2).collect::<Vec<&str>>().join(" ");
     match value.parse::<i64>() {
         Ok(parsed) => Create(value_name, Int(parsed)),
         Err(_e) => Create(value_name, Line(value)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::appropriation;
+    use crate::opcode::OpCode::*;
+    use crate::opcode::ValueType::*;
+
+    #[test]
+    fn test_appropriation_int() {
+        let data = vec![">", "a", "1"];
+        let result = appropriation(data);
+        assert_eq!(result, Create("a".to_string(), Int(1)));
+    }
+
+    #[test]
+    fn test_appropriation_line() {
+        let data = vec![">", "a", "1d"];
+        let result = appropriation(data);
+        assert_eq!(result, Create("a".to_string(), Line("1d".to_string())));
+    }
+
+    #[test]
+    fn test_appropriation_long_line() {
+        let data = vec![">", "a", "1d", "dsad", "123"];
+        let result = appropriation(data);
+        assert_eq!(
+            result,
+            Create("a".to_string(), Line("1d dsad 123".to_string()))
+        );
+    }
+
+    #[test]
+    fn test_appropriation_edge_one() {
+        let data = vec![">", "a"];
+        let result = appropriation(data);
+        assert_eq!(
+            result,
+            ErrorCode("the operation is not specified correctly".to_string())
+        );
+    }
+
+    #[test]
+    fn test_appropriation_edge_two() {
+        let data = vec![">"];
+        let result = appropriation(data);
+        assert_eq!(
+            result,
+            ErrorCode("the operation is not specified correctly".to_string())
+        );
     }
 }
