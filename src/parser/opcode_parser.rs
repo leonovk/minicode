10f3d02c5b480d7b0f2c72f511297b066272d36b
Kirill Leonov <leonov835@yandex.ru>
2024-01-21 03:29:33 +0300

    need more tests

--- src/parser/opcode_parser.rs
+++ src/parser/opcode_parser.rs
@@ -9,35 +9,67 @@ use super::print::print;
 use super::user_var::user_var;
 
 pub fn get_opcode(line: &String) -> OpCode {
-    let parts: Vec<&str> = line.split_whitespace().collect();
-    let command = parts[0];
+    let data: Vec<&str> = line.split_whitespace().collect();
+    let command = data[0];
 
-    if command == ">" {
-        appropriation(parts)
-    } else if command == "p" {
-        print(parts)
-    } else if command == "f" {
-        file(parts)
-    } else if command == "$>" {
-        user_var(parts)
-    } else if command == "=" {
-        calculation(parts)
-    } else if command == "?" {
-        condition(parts)
-    } else {
-        ErrorCode("Could not recognize the command".to_string())
+    match command {
+        ">" => appropriation(data),
+        "p" => print(data),
+        "f" => file(data),
+        "$>" => user_var(data),
+        "=" => calculation(data),
+        "?" => condition(data),
+        _ => ErrorCode("Could not recognize the command".to_string()),
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::opcode::OperationType::*;
     pub use crate::opcode::ValueType::*;
     use crate::parser::opcode_parser::*;
 
     #[test]
-    fn test_get_opcode() {
+    fn test_get_opcode_create() {
         let ex1 = "> a 32".to_string();
 
         assert_eq!(get_opcode(&ex1), Create("a".to_string(), Int(32)));
+    }
+
+    #[test]
+    fn test_get_opcode_print() {
+        let ex1 = "p a".to_string();
+
+        assert_eq!(get_opcode(&ex1), Print("a".to_string()));
+    }
+
+    #[test]
+    fn test_get_opcode_file() {
+        let ex1 = "f a test/test_file.txt".to_string();
+
+        assert_eq!(
+            get_opcode(&ex1),
+            Create("a".to_string(), Line("first\nsecond".to_string()))
+        );
+    }
+
+    #[test]
+    fn test_get_opcode_calc() {
+        let ex1 = "= a + 1".to_string();
+
+        assert_eq!(
+            get_opcode(&ex1),
+            Operation("a".to_string(), Increment, Int(1))
+        );
+    }
+
+    #[test]
+    fn test_get_opcode_condition() {
+        let ex1 = "? a = 1 6".to_string();
+
+        assert_eq!(
+            get_opcode(&ex1),
+            Condition("a".to_string(), Int(1), true, 6)
+        );
     }
 }
